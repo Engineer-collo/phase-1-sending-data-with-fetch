2a672f1ff839ce3843c3b856960abfcf6803404d
Engineer-Collo <collinslikhomba@gmail.com>
2025-01-08 08:46:13 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+//console.log("Hello world!")
 // Add your code here
+function submitData(userName="collins",userEmail="collinslikhomba@.gmail.com") { 
+    
+  return  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify( {userName:`${userName}`, userEmail:`${userEmail}` })
+   })
+ .then(res => res.json())
+ .then(data => console.log(data))
+.catch(error => 
+    console.error("Error:", error)
+)
+}
+submitData();
+
